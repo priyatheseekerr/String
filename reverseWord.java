afe36045421a91be3aed51c53719299a57b23512
theseekerrr <priyaseekerrr@gmail.com>
2025-03-31 19:38:04 +0530

    Reverse words in a string

---
+++ reverseWord.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class reverseWord {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter a String: ");
+        String word=sc.nextLine();
+        
+        String[] str=word.split(" ");
+         for(int i=0;i<str.length;i++){
+            StringBuilder wrd=new StringBuilder(str[i]);
+            wrd.reverse();
+            str[i]=wrd.toString();
+         }
+         System.out.println(String.join(" ", str));
+    }
+}
